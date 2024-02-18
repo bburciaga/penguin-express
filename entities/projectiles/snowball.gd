2a1f0d2757a235c453a2a041a9b0aa4bfc32dd16
bburciaga <burciagab16@gmail.com>
2024-02-17 21:59:30 -0800

    refactor for snowball

--- entities/projectiles/snowball.gd
+++ entities/projectiles/snowball.gd
@@ -2,25 +2,19 @@ class_name Snowball
 
 extends Area2D
 
-const SPEED: float = 150.0
+const SPEED: float = -150.0
 
 var ATTACK: Attack = Attack.new(1, self.global_position, 2.5)
 
 func _physics_process(delta):
-	position += transform.x * SPEED * delta
+	position += transform.y * SPEED * delta
 
 func _on_area_entered(area: Area2D):
-	print(area)
-	var entity = area.get_parent()
-	print(entity.name)
 	if area is HitboxComponent:
 		var hitbox: HitboxComponent = area
 		if area.is_in_group("Enemy"):
 			hitbox.damage(ATTACK)
-			queue_free()
-		if area.is_in_group("Projectile"):
-			queue_free()
-		if area.is_in_group("Object"):
+		if not area.is_in_group("Player"):
 			queue_free()
 
 func _on_screen_exited():
