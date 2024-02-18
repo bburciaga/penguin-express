4ea2eea5a5907c6b02118447f1578417933d2226
A-dith-ya <awedithya@gmail.com>
2024-02-18 11:07:48 -0800

    Implement stop bear spawn during stopped time

--- utils/EnemyMovement.gd
+++ utils/EnemyMovement.gd
@@ -1,33 +1,16 @@
 extends Area2D
 
-var speed = 200
-var direction = Vector2(1, 1)
+var speed: float = 200.0
+var direction: Vector2 = Vector2(1, 1)
 
-var color_rect = null
-var player = null
-
-func _ready():
-	color_rect = $"../ColorRect"
-	player = $"../CharacterBody2D"
+@onready var color_rect: Node = $"../ColorRect"
+@onready var player: Node = $"../Player"
 	
-func _physics_process(delta):
-	if player != null:
-		var distance_to_player = position.distance_to(player.position)
-		if distance_to_player < 100:
-			var x_direction = 0
-			if player.position.x - position.x != 0:
-				x_direction = (player.position.x - position.x) / abs(player.position.x - position.x)
-			direction = Vector2(x_direction, 1)
-		else:
-			direction = (player.position - position).normalized()
-	position += direction * speed * delta
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = (player.global_position - self.global_position).normalized()
+	position += Vector2(direction.x, 1) * speed * delta
 	_check_bounds()
 
-func _check_bounds():
-	if color_rect != null:
-		var rect_position = color_rect.position
-		var rect_size = color_rect.size
-		if position.x < rect_position.x or position.x > rect_position.x + rect_size.x:
-			direction.x = -direction.x
-		if position.y < rect_position.y or position.y > rect_position.y + rect_size.y:
-			self.queue_free()
+func _check_bounds() -> void:
+	if position.y < color_rect.position.y or position.y > color_rect.position.y + color_rect.size.y:
+		self.queue_free()
